6a464973661611e44e6742a056503dd43ecc3e67
Adam Bradbury <f.adambrad@gmail.com>
2017-10-13 14:50:18 -0600

    GET - DELETE added w err continuing

--- test/test-recipies.js
+++ test/test-recipies.js
@@ -33,5 +33,44 @@ describe('Recipes', function() {
         });
     });
 
+    it('should list new recipe item on POST ', function(){
+        const newItem = {name: 'salad', ingredients: ['tomatoes, mozarella']};
+        return chai.request(app)
+            .post('/recipes')
+            .send(newItem)
+            .then(function(res){
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.include.keys('id', 'name', 'ingredients');
+            res.body.id.should.not.be.null;
+            res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
 
+        });
+    });
+    it('should list newly added recipe items on PUT', function(){
+        const newIngredient = {ingredient: ['basil, Olive oil']};
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res){
+                newIngredient.id = res.body[0].id;
+                return chai.request(app)
+                .put(`/recipes/${newIngredient.id}`)
+                .send(newIngredient)
+        })
+            .then(function(res){
+                res.should.have.status(204);
+            });
+    it('should remove a list item with delete', function(){
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res){
+                return chai.request(app)
+                .delete(`/recipes/${res.body[0].id}`);
+            })
+            .then(function(res){
+                res.should.have.status(204);
+            });
+         });
+    });
 });
